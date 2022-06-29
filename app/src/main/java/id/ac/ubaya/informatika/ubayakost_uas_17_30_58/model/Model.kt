1fab90f304eda6fcbca1b46af44d2e024632d358
nigelwilliam16 <s160419017@student.ubaya.ac.id>
2022-06-29 11:41:22 +0700

    UAS 2 (Nigel)

--- app/src/main/java/id/ac/ubaya/informatika/ubayakost_uas_17_30_58/model/Model.kt
+++ app/src/main/java/id/ac/ubaya/informatika/ubayakost_uas_17_30_58/model/Model.kt
@@ -1,18 +1,57 @@
-package id.ac.ubaya.informatika.ubayakost_uas_17_30_58.model
+package id.ac.ubaya.informatika.ubayakost_160419017.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
+data class Account(
+    @ColumnInfo(name = "username")
+    var username:String,
+    @ColumnInfo(name = "email")
+    var email:String,
+    @ColumnInfo(name = "password")
+    var password:String,
+    @ColumnInfo(name = "name")
+    var name:String,
+    @ColumnInfo(name = "phoneNumber")
+    var phoneNumber:String,
+    @PrimaryKey(autoGenerate = true)
+    var accountID:Int =0
+)
 
+@Entity
 data class Kost(
-    var id: String?,
-    var namaKost: String?,
-    var deskripsi: String?,
-    var tipeKost: String?,
-    var alamat:String?,
-    var harga:String?,
-    var telepon:String?,
-    var message:String?,
-    var pemilik:String?,
-    var rekening:String?,
-    var foto:String?
+    @ColumnInfo(name = "namaKost")
+    var namaKost: String,
+    @ColumnInfo(name = "deskripsi")
+    var deskripsi: String,
+    @ColumnInfo(name = "tipeKost")
+    var tipeKost: String,
+    @ColumnInfo(name = "alamat")
+    var alamat:String,
+    @ColumnInfo(name = "harga")
+    var harga:String,
+    @ColumnInfo(name = "telepon")
+    var telepon:String,
+    @ColumnInfo(name = "pemilik")
+    var pemilik:String,
+    @ColumnInfo(name = "rekening")
+    var rekening:String,
+    @ColumnInfo(name = "foto")
+    var foto:String,
+    @PrimaryKey(autoGenerate = true)
+    var idKost: Int,
+)
+
+@Entity
+data class KamarKost(
+    @ColumnInfo(name = "fasilitas")
+    var fasilitas:String,
+    @ColumnInfo(name = "tipeKamar")
+    var tiperKamar:String,
+    @PrimaryKey(autoGenerate = false)
+    var nomorKamar: String,
 )
